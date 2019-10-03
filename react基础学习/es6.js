3d7fa07042fad92a707a843cfb2b872f0cd7d4ca
facefall <3108999582@qq.com>
2019-10-03 10:46:12 +0800

    优化目录 将对react的学习内容放到一个单独的文件件中
    Signed-off-by: facefall <3108999582@qq.com>

---
+++ react基础学习/es6.js
@@ -0,0 +1,13 @@
+export const title = "title";
+export const arr = ['<li>1</li>','<li>2</li>','<li>3</li>'];
+export const json = {
+    name : '海绵宝宝',
+    age : 18
+};
+export function getDate() {
+    return 'getData'
+}
+
+export default {
+    name : '派大星'
+}
