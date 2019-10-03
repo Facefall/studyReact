36302ee86c1a2fdc0640ef282ee0678d963253ad
facefall <3108999582@qq.com>
2019-10-03 19:10:12 +0800

    todolist重构第四步,完善组件间的传递函数,数据的通讯功能 实现todolist的添加,删除,标记是否完成的功能. Signed-off-by: facefall <3108999582@qq.com>

--- react小案例/todomvc-app-template/src/app.jsx
+++ react小案例/todomvc-app-template/src/app.jsx
@@ -1,10 +1,77 @@
 window.App = class App extends React.Component{
+	constructor(props) {
+		super(props);
+
+	}
+	state = {
+		list:[
+			{
+				name:'海绵宝宝',
+				id:Math.random(),
+				is_finish:false
+			},
+			{
+				name:'派大星',
+				id:Math.random(),
+				is_finish:false
+			},
+			{
+				name:'蟹老板',
+				id:Math.random(),
+				is_finish:false
+			}
+		]
+	};
+
+	handleRemoveItem = (index) => {
+		let list = this.state.list;
+		list.splice(index,1);
+		this.setState({
+			list:list
+		});
+	};
+
+	handleKeyDown = (e) => {
+		// console.log(e);
+		console.log(123);
+		const {target,keyCode} = e;
+		if (keyCode !== 13)	return;
+
+
+		let list = this.state.list;
+		let txt = target.value.trim();
+		if (txt.length == 0)	return;
+
+		list.push({
+			name : txt,
+			id : Math.random(),
+			is_finish:false
+		});
+		this.setState({
+			list:list
+		});
+		target.value = "";
+	};
+
+	handleFinish = (index)=>{
+		const list = this.state.list;
+		list[index].is_finish = !list[index].is_finish;
+		this.setState({
+			list
+		});
+	};
 	render() {
 		return (
 			<div>
 				<section className="todoapp">
-					<Head></Head>
-					<Main></Main>
+					<Head
+						KeyDown={this.handleKeyDown}
+					></Head>
+					<Main
+						Finish={this.handleFinish}
+						handleRemoveItem={this.handleRemoveItem}
+						data={this.state}
+					></Main>
 				</section>
 				<Info></Info>
 			</div>
--- react小案例/todomvc-app-template/src/component/head/index.jsx
+++ react小案例/todomvc-app-template/src/component/head/index.jsx
@@ -1,9 +1,21 @@
 window.Head = class Head extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			onKeyDown : props.KeyDown
+		}
+	}
+
 	render() {
 		return (
 			<header className="header">
 				<h1>todos</h1>
-				<input className="new-todo" placeholder="What needs to be done?" autoFocus  />
+				<input
+					// onKeyDown={this.state.onKeyDown}
+					onKeyDown={this.props.KeyDown}
+					className="new-todo"
+					placeholder="What needs to be done?"
+					autoFocus  />
 			</header>
 		);
 	}
