b3c0e0891c56635d87f8020b2fc147116720fc4d
facefall <3108999582@qq.com>
2019-10-04 10:19:39 +0800

    react-toggleList组件练习,将数据展现在视图上
    Signed-off-by: facefall <3108999582@qq.com>

---
+++ react-toggleList组件/data.js
@@ -0,0 +1,47 @@
+data = {
+    "study":{
+        "name":"前端",
+        "list":[
+            {
+                id:Math.random(),
+                "name":"react",
+                "intro":"javascript ui lib"
+            },
+            {
+                id:Math.random(),
+                "name":"jquery",
+                "intro":"easy javascript lib"
+            },
+            {
+                id:Math.random(),
+                "name":"node",
+                "intro":"an js platform "
+            }
+        ]
+    },
+    "netFriend":{
+        "name":"海绵宝宝",
+        "list":[
+            {
+                id:Math.random(),
+                "name":"海绵宝宝",
+                "intro":"喜欢蟹黄堡"
+            },
+            {
+                id:Math.random(),
+                "name":"派大星",
+                "intro":"喜欢海绵宝宝"
+            },
+            {
+                id:Math.random(),
+                "name":"章鱼哥",
+                "intro":"讨厌海绵宝宝"
+            },
+            {
+                id:Math.random(),
+                "name":"蟹老板",
+                "intro":"喜欢钱"
+            }
+        ]
+    }
+};
