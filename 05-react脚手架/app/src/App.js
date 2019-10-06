dd8e20fc0bc623dc894f8c5a87abc6e816750e3a
facefall <3108999582@qq.com>
2019-10-06 11:55:58 +0800

    更新react脚手架目录,格式化代码
    Signed-off-by: facefall <3108999582@qq.com>

--- 05-react脚手架/app/src/App.js
+++ 05-react脚手架/app/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Head from "./component/head/index.jsx";
+import Main from "./component/main/index.jsx";
+import Foot from "./component/foot/index.jsx";
+
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <Head></Head>
+                        <Main></Main>
+                        <Foot></Foot>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
---
+++ 05-react脚手架/app/src/component/foot/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Foot extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Foot组件</h3>
+            </div>
+        );
+    }
+}
+
+export default Foot;
